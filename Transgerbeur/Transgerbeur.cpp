ac7397c103fb6d8a0c3ddb956a46fec9181adb28
fedi <fedizitouni6@gmail.com>
2024-12-26 16:42:02 +0100

    #commit 3

--- Transgerbeur/Transgerbeur.cpp
+++ Transgerbeur/Transgerbeur.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-enum EEtat { vide, palette, rouleau };
+enum class  EEtat { vide=0, palette=1, rouleau=2  };
 
 struct SPalette {
 	int poids;
@@ -15,7 +15,10 @@ struct SRouleau {
 	int numero;
 };
 
-
+union UContenu {
+	SPalette pal;
+	SRouleau roul;
+};
 struct SCasier {
 	int px;
 	int py;
@@ -24,10 +27,7 @@ struct SCasier {
 };
 
 
-union UContenu {
-	SPalette pal;
-	SRouleau roul;
-};
+
 
 
 
@@ -37,14 +37,19 @@ void menuTransgerbeur() {
 	SRouleau  ro1;
 	SPalette pa1;
 	UContenu cont1;
+	ro1.longueur = 1;
+	ro1.numero = 2;
 	pa1.poids = 20;
 	strcpy_s(pa1.reference, "The ric veut djin"); // ignisalisation des chaine de caractere
 	EEtat etatC1;
-	etatC1 = vide;
+	etatC1= EEtat::vide ;
 	cont1.pal = pa1;
 	cont1.roul = ro1;//
 	SCasier c1;
 	SCasier c2;
+	c2.etat = etatC1;
+	c2.px = 1;
+	c2.py = 2;
 	c1.cont = cont1;
 	c1.etat = etatC1;//
 	c1.px = 2;
@@ -52,5 +57,9 @@ void menuTransgerbeur() {
 	c2.cont = cont1;
 	tabCasier[0] = c1;
 	tabCasier[2] = c2;
-
+	cout << endl << " la taille est de : " <<  sizeof(ro1);
 }
+int main() {
+	menuTransgerbeur(); // Call the menu function
+	return 0;
+}	
