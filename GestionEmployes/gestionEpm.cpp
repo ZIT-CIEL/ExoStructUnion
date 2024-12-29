17545a3011d5be0ed676cb60d4effe6ae2407c29
fedi <fedizitouni6@gmail.com>
2024-12-29 18:36:35 +0100

    #commit7

--- GestionEmployes/gestionEpm.cpp
+++ GestionEmployes/gestionEpm.cpp
@@ -2,10 +2,25 @@
 #include<iostream>
 
 using namespace std;
+struct date {
 
-void menugestionEmp()
-{
-	cout << "Bienvenue dans le menu gestionEmp";
+	int jour; 
+	int mois; 
+	int	annee; 
 
-
+};
+bool verifierDate(date d) {
+    // Vérifie que le mois est entre 1 et 12
+    if (d.mois < 1 || d.mois > 12) {
+        return false;
+    }
+    // Vérifie que le jour est entre 1 et 30
+    if (d.jour < 1 || d.jour > 30) {
+        return false;
+    }
+    // L'année peut être >= 0
+    if (d.annee < 0) {
+        return false;
+    }
+    return true;
 }
